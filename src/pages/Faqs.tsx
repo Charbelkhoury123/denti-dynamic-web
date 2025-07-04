b996db32823926cdeb2e8762e14e1f19b7a8f40b
CharbelKh <Charbelkhoury1121@gmail.com>
2025-07-05 00:36:41 +0300

    fixes

--- src/pages/Faqs.tsx
+++ src/pages/Faqs.tsx
@@ -129,7 +129,7 @@ export default function Faqs() {
                 Contact Us
               </motion.a>
             </div>
-          </div>
+          </motion.div>
         </div>
       </motion.div>
     </main>
